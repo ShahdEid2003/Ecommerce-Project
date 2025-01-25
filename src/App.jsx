bcede7f284fbe487c02f2eca1e09b7999f0ec26a
ShahdEid2003 <shahdeid012@gmail.com>
2025-01-25 23:33:15 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import AuthLayout from './layouts/AuthLayout.jsx'
 import DashLayout from './layouts/DasboardLayout.jsx'
-import Register from './pages/user/register/register.jsx'
-import Login from './pages/user/login/login.jsx'
-
+import Login from './pages/user/login/Login.jsx'
+import Register from './pages/user/register/Register.jsx'
 export default function App() {
   const router= createBrowserRouter(
     [
