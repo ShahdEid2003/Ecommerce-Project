a66b77757a60675c6f1e2ce192cf0a0d6ebcdcd3
ShahdEid2003 <shahdeid012@gmail.com>
2025-02-21 19:57:50 +0200

    update

--- src/pages/user/profile/Info.jsx
+++ src/pages/user/profile/Info.jsx
@@ -1,16 +1,22 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../../components/user/context/UserContext";
 import Loader from "../../../components/loader/loader";
-export default function info() {
+
+export default function Info() {
   const { user, loading } = useContext(UserContext);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (!user) {
+    return <p>Unable to fetch user information. Please try again later.</p>;
+  }
+
   return (
-    <>
-      {loading ? <Loader /> : null}
-      <section className="p-5 ms-3">
-        <p>User Name: {user.userName}</p>
-        <p>Email:{user.email}</p>
-        
-      </section>
-    </>
+    <section className="p-5 ms-3">
+      <p>User Name: {user.userName}</p>
+      <p>Email: {user.email}</p>
+    </section>
   );
 }
